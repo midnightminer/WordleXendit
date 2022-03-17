2a9a9c45cfea238a883062fe01fbee5e4c8b7e05
Adhi Faizal <faizal.maf@gmail.com>
2022-03-17 10:39:18 +0700

    Initial Commit

---
+++ WordleXendit/WordleXenditApp.swift
@@ -0,0 +1,20 @@
+//
+//  WordleXenditApp.swift
+//  WordleXendit
+//
+//  Created by Faizal on 17/03/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleXenditApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
